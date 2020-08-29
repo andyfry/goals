42771841205d051bf89283a929087d7f73db519d
Andy Fry <andrewdalefry@gmail.com>
2020-08-29 04:55:54 -0500

    Added a simple stats page

--- README.md
+++ README.md
@@ -10,9 +10,20 @@ goals --today            ( Show Goals for Today )
 goals --yesterday        ( Show Goals for Yesterday )
 goals --tomorrow         ( Show Goals for Tomorrow )
 goals --date 5/4/1979    ( Show Goals for Given Date ) *Not Supported Yet*
+goals --stats            ( Show Stats )
 
 
 # TODO
+- Switch to Async
 - Create a command to add an item - today vs tomorrow
 - Create a way to keep it visually up to date
 - Create a way to complete a goal
+- Better interface. Like git or homebrew
+
+goals
+goals --tomorrow
+goals add "Meditation"
+goals add "Meditation" --today
+goals stats
+goals done 1
+goals done 1 --yesterday
--- goals.ts
+++ goals.ts
@@ -1,23 +1,21 @@
 import { parse } from "https://deno.land/std/flags/mod.ts";
-import { exists, existsSync, readJson, readJsonSync } from "https://deno.land/std/fs/mod.ts";
+import {
+	exists,
+	existsSync,
+	readJson,
+	readJsonSync,
+	walkSync
+} from "https://deno.land/std/fs/mod.ts";
 
 const monthNames = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
 
 async function main() {
 	const args = parse(Deno.args);
 
-	const today = new Date();
-	const yesterday = new Date(Date.now() - 86400000);
-	const tomorrow = new Date(Date.now() + 86400000);
-
-	if (args.yesterday) {
-		displayGoals(yesterday);
-	}
-	if (args.today || (!args.yesterday && !args.tomorrow)) {
-		displayGoals(today);
-	}
-	if (args.tomorrow) {
-		displayGoals(tomorrow);
+	if (args.stats) {
+		displayStats();
+	} else {
+		displayGoals(args);
 	}
 }
 
@@ -33,7 +31,23 @@ function buildPath(date: Date) {
 	return `${basePath}/${year}/${month}/${day}/${fileName}`;
 }
 
-function displayGoals(date: Date) {
+function displayGoals(args: any) {
+	const today = new Date();
+	const yesterday = new Date(Date.now() - 86400000);
+	const tomorrow = new Date(Date.now() + 86400000);
+
+	if (args.yesterday) {
+		displayDailyGoals(yesterday);
+	}
+	if (args.today || (!args.yesterday && !args.tomorrow)) {
+		displayDailyGoals(today);
+	}
+	if (args.tomorrow) {
+		displayDailyGoals(tomorrow);
+	}
+}
+
+function displayDailyGoals(date: Date) {
 	const path = buildPath(date);
 
 	if (!existsSync(path)) {
@@ -57,6 +71,43 @@ function displayGoals(date: Date) {
 
 function displayGoal(goal: goal) {
 	console.log(`  [${goal.done ? 'X' : ' '}] ${goal.title}`);
+}
+
+function displayStats() {
+	const basePath = '/Users/andyfry/Projects/daily/goals';
+	console.log("Goals Statistics");
+	let numberOfDays = 0;
+	let numberOfGoals = 0;
+	let numberOfGoalsCompleted = 0;
+	let numberOfSuccessfulDays = 0;
+
+
+
+	for (const entry of walkSync(basePath)) {
+		if (entry.name === 'goals.json') {
+			let success = true;
+			numberOfDays++;
+			const goals = readJsonSync(entry.path) as goal[];
+			numberOfGoals += goals.length;
+			goals.forEach((goal)=>{
+				if(goal.done){
+					numberOfGoalsCompleted++;
+				} else {
+					success = false;
+				}
+
+			});
+			if(success){
+				numberOfSuccessfulDays++;
+			}
+
+		}
+	}
+
+	console.log('Number of Days: ', numberOfDays);
+	console.log( numberOfGoalsCompleted, '/', numberOfGoals);
+	console.log('Successful Days: ', numberOfSuccessfulDays);
+	
 }
 
 function formatDate(date: Date): string {
